b3f98e4cdcec3711119c2f6eaafddb522bd96f58
Lukas Veiga <lukas.veiga@hotmail.com>
2024-04-18 10:40:37 -0300

    Refact: Change one arg into mesure time function to use varargs instead + wrape test logic into a fuction inside of utils package

--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 import time;
 
-def mesureFunctionTimeExecution(function, input):
+def mesureFunctionTimeExecution(function, *args):
     start = time.time();
-    function(input);
+    function(*args);
     end = time.time();
     return end - start;
+
+def checkFunctionResult(given, expected, message):
+    if (given != expected):
+       raise Exception(message);
